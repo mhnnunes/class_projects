10d35c1b77de73af98a258d20cc96ec89c205599
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2018-08-30 22:23:26 -0300

    Add testing script

---
+++ heuristics/implementation/ti_1/exec.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CURDIR=`pwd`
+echo $CURDIR
+
+DATASETS=$HOME/git/class_projects/heuristics/implementation/dataset/
+echo $DATASETS
+
+RESULT_FILE=$CURDIR/out_time.log
+
+# Clean out old result file
+echo "" > $RESULT_FILE
+
+# Begin test
+for f in `find $DATASETS -name *.tsp|sort`;
+do
+  echo "Testing file: ${f}"
+
+  # Pre-processing input file
+  # Remove space between the word DIMENSION and ':'
+  # sed -i "s/DIMENSION\ :/DIMENSION:/g" $f
+  # head -n6 "${f}" | grep DIME
+  # Verify if the first 6 lines of every file are header lines
+  # head -n6 "${f}" | grep COORD
+  # Verify the syntax of the lines denoting the distance type
+  # head -n6 $f | grep WEIGHT
+  # sed -i "s/EDGE_WEIGHT_TYPE\ :/EDGE_WEIGHT_TYPE:/g" $f
+
+  # Execute and save results from program
+  TIME_START=`date +%s.%N`
+  OUTPUT=`$CURDIR/tp1 < $f`
+  TIME_END=`date +%s.%N`
+  TIME_DIFF=`echo ${TIME_END} - ${TIME_START}| bc`
+  echo "${OUTPUT} ${f} ${TIME_DIFF}" >> $RESULT_FILE
+done
